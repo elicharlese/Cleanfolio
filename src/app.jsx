bc249e2ce3ee8598a984c9ebf931e93ca688449a
elicharlese <elicharles.e@gmail.com>
2022-08-04 11:40:46 -0700

    modified:   src/app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,2 @@
-ReactDOM.render(myElement, document.getElementById('root'));
+// ReactDOM.render(myElement, document.getElementById('root')); // Render the component inside the div with id="root"
+// TODO: Add the rest of the following functions
